2400c4779e976d80faf5d1697fd140ad41149c33
EDDY ESCALANTE USTARIZ <eescalante@ucb.edu.bo>
2025-08-15 10:56:10 -0400

    V2

--- APRENDIENDO JS/aprendiendo.js
+++ APRENDIENDO JS/aprendiendo.js
@@ -5,9 +5,18 @@ function mi_primer_algoritmo(){
     alert(mensaje);
 }
 
-function convertirBinDec(){
-    ///codigo
-    //codigo
+function decimalABinario(binario){
+   var decimal = 0;
+   for (let i = 0; i < binario.length; i++) {
+        digitoBinario = binario[i];
+        if(digitoBinario === '1'){
+            decimal = decimal * 2 + 1;
+        }
+        else{
+            decimal = decimal * 2 + 0;
+        }
+   }
+   alert(decimal);
 }
 
 function sumaPrimos(){
